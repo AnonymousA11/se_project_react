2c2851bd21255582e095b8a0a65ff04b1d4c4bb9
AnonymousA11 <mccullohj@gmail.com>
2026-03-18 21:48:00 -0400

    no update

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import "../blocks/Header.css";
+import logo from "/src/images/Logo.svg";
+import avatar from "/src/images/Avatar.png";
+
+function Header({ onAddButtonClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img className="header__logo" src={logo} alt="Logo" />
+        <p className="header__date-and-location">
+          {currentDate}, {weatherData.city}
+        </p>
+      </div>
+
+      <div className="header__user-container">
+        <button
+          className="header__add-clothes-button"
+          onClick={onAddButtonClick}
+          type="button"
+        >
+          +Add clothes
+        </button>
+        <p className="header__user-name">Terrence Tegegne</p>
+      </div>
+      <img className="header__user-avatar" src={avatar} alt="User Avatar" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/ModalWithForm.jsx
@@ -0,0 +1,39 @@
+import "../blocks/ModalWithForm.css";
+import closeIcon from "/src/images/modal__close-btn.png";
+
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  isOpen,
+  handleCloseClick,
+}) {
+  return (
+    <div
+      name="add_garment"
+      className={`modal ${isOpen ? "modal__opened" : ""}`}
+    >
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button
+          onClick={handleCloseClick}
+          className="modal__close-btn"
+          type="button"
+        >
+          {" "}
+          <img src={closeIcon}></img>
+        </button>
+        <form className="modal__form">
+          {children}
+
+          <button className="modal__submit-button" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
